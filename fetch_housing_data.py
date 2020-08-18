70b6bb4f6439bfd3aa78193b924ab6ad1901855d
Mauro Yone <55468244+mauroyone@users.noreply.github.com>
2020-08-18 19:37:58 -0300

    Add files via upload

---
+++ fetch_housing_data.py
@@ -0,0 +1,24 @@
+'''Function to download and decompress data from a particular URL
+
+DOWNLOAD_ROOT: url of the site where the .tar is located'''
+
+import os
+import tarfile
+import urllib.request
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    '''This function creates the dir, downloads the .tar and decompress it'''
+
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+if __name__ == '__main__':
+    fetch_housing_data()
